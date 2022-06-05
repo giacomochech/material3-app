e2c36284de3001d7ba12d53b9c380ce530a4b80f
tao27 <moscerino3@gmail.com>
2022-06-05 16:52:12 +0200

    Creato schema DataBase

---
+++ app/src/main/java/com/example/material3app/data/Food.kt
@@ -0,0 +1,12 @@
+package com.example.material3app.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "food_table")
+data class Food(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int,
+    val nome: String,
+    val calorie: Int
+)
---
+++ app/src/main/java/com/example/material3app/data/FoodDao.kt
@@ -0,0 +1,15 @@
+package com.example.material3app.data
+
+import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+interface FoodDao
+{
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addFood(food:Food)
+
+    @Query("SELECT * FROM food_table ORDER BY id ASC")
+    fun readAllData(): LiveData<List<Food>>
+}
