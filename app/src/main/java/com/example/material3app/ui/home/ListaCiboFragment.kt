6b7271a3c3e8251e3a296122f8ed264cf40eec33
Umberto-Salviati <u.salviati@gmail>
2022-06-05 11:56:59 +0200

    cambiati alcuni colori errori

--- app/src/main/java/com/example/material3app/ui/home/ListaCiboFragment.kt
+++ app/src/main/java/com/example/material3app/ui/home/ListaCiboFragment.kt
@@ -1,5 +1,6 @@
 package com.example.material3app.ui.home
 
+import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,38 +9,33 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.cardview.widget.CardView
+import androidx.core.content.res.ResourcesCompat
 import com.example.material3app.R
-import java.time.LocalDateTime
-import java.time.Month
-import java.time.MonthDay
+import com.example.material3app.ui.Material3AppTheme
+import com.google.android.material.color.MaterialColors
+import java.time.LocalDate
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
  * Use the [ListaCiboFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListaCiboFragment(giorno: Int, mese: Month, KcalAssunte :Int) : Fragment() {
-    // TODO: Rename and change types of parameters
-  /*  private var param1: String? = null
-    private var param2: String? = null*/
-    val day = giorno
-    val month = mese
-    val KcalAssunte = KcalAssunte
+@RequiresApi(Build.VERSION_CODES.O)
+class ListaCiboFragment(date : LocalDate, private val KcalAssunte: Int) : Fragment() {
+
+
+    private val day = date.dayOfMonth
+    private val month = date.month
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-          /*  param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)*/
-        }
     }
 
 
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -49,6 +45,11 @@ class ListaCiboFragment(giorno: Int, mese: Month, KcalAssunte :Int) : Fragment()
         val textKcalAssunte : TextView = view.findViewById(R.id.KcalAssunte)
         val textGiorno : TextView = view.findViewById(R.id.giorno_del_mese)
         val textMese : TextView = view.findViewById(R.id.mese)
+        var card : CardView = view.findViewById(R.id.cardView)
+      if(KcalAssunte> 2000){//TODO: Obiettivo
+          card.setCardBackgroundColor(ResourcesCompat.getColor(resources, com.google.android.material.R.color.m3_sys_color_dark_error,null))
+          textKcalAssunte.setTextColor(ResourcesCompat.getColor(resources, com.google.android.material.R.color.m3_sys_color_dark_on_error,null))
+      }
         textKcalAssunte.text = KcalAssunte.toString()
 
         textGiorno.text = day.toString()
@@ -59,23 +60,8 @@ class ListaCiboFragment(giorno: Int, mese: Month, KcalAssunte :Int) : Fragment()
         return view
     }
 
-    /*companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListaCiboFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            /*ListaCiboFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }*/
-            }
-    }*/
+
+
+
+
 }
