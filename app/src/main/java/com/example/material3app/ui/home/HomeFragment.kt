6faa5db8b3f4ae90c7683d463e8c5fdc112659df
Umberto-Salviati <u.salviati@gmail>
2022-06-05 11:56:59 +0200

    cambiati alcuni colori errori

--- app/src/main/java/com/example/material3app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/material3app/ui/home/HomeFragment.kt
@@ -2,8 +2,6 @@ package com.example.material3app.ui.home
 
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,13 +10,13 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.material3app.databinding.FragmentHomeBinding
 import java.time.LocalDate
-import java.util.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,32 +27,44 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(inflater, container, false)// infalete(r.layoutfragment home...)
         val root: View = binding.root
 
-
+        var map : Map<LocalDate,Int>
+        //TODO() : riempi da database
+        map = riempiMap()
 
         val viewPager = binding.ViewPager//r.id.text_home
-        viewPager.adapter = AdapterViewPager()
+        viewPager.adapter = AdapterViewPager(map)
         viewPager.setCurrentItem(Int.MAX_VALUE/2,false)
 
         return root
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun riempiMap(): Map<LocalDate, Int> {
+        var mappa = mutableMapOf<LocalDate,Int>()
+        for(i in 1..10){
+            mappa[LocalDate.now().plusDays((i-5).toLong())] = (2000+i*1.25).toInt()
+
+        }
+        return mappa
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
 
     }
-    inner class AdapterViewPager : FragmentStateAdapter(childFragmentManager,lifecycle){
+    inner class AdapterViewPager(private val map: Map<LocalDate, Int>) : FragmentStateAdapter(childFragmentManager,lifecycle){
         override fun getItemCount(): Int {
           return Int.MAX_VALUE
         }
 
         @RequiresApi(Build.VERSION_CODES.O)
         override fun createFragment(position: Int): Fragment {
+            val key = LocalDate.now().plusDays((position-Int.MAX_VALUE/2).toLong())
 
-
-            val day =  LocalDate.now().plusDays((position-Int.MAX_VALUE/2).toLong()).dayOfMonth
-            val month = LocalDate.now().plusDays((position-Int.MAX_VALUE/2).toLong()).month
-
-            return ListaCiboFragment(day,month,1000)
+            if(map.containsKey(key)) {
+                return map[key]?.let { ListaCiboFragment(key, it) }!!
+            }
+            return ListaCiboFragment(key,0)
         }
 
 
